9addfb8a738b1b6c1d398a93565dabec45ac6db1
joyawus <121011722+joyawus@users.noreply.github.com>
2022-12-21 21:58:38 +0100

    Update joynotebook1.py

--- joynotebook1.py
+++ joynotebook1.py
@@ -1,6 +1,6 @@
 import pandas as pd
   df=pd.read_csv("exchange_rates.csv")
   df.rename( columns={'Unnamed: 0':'Pounds'}, inplace=True ) 
-df1=df.loc[df["Pounds"] == 'GBP']
- df1
- exchange_rate=float(df1["Rates"].values) print(exchange_rate)   
+  df1=df.loc[df["Pounds"] == 'GBP']
+  df1
+  exchange_rate=float(df1["Rates"].values) print(exchange_rate)   
