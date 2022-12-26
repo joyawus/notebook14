0e6fdf72f951f7a663e27053638e8073878fa6b7
joyawus <121011722+joyawus@users.noreply.github.com>
2022-12-26 19:50:56 +0100

    Update joynotebook.py

--- joynotebook.py
+++ joynotebook.py
@@ -1,6 +1,5 @@
-import pandas as pd
-  df=pd.read_csv("exchange_rates.csv")
-  df.rename( columns={'Unnamed: 0':'Pounds'}, inplace=True ) 
-df1=df.loc[df["Pounds"] == 'GBP']
- df1
- exchange_rate=float(df1["Rates"].values) print(exchange_rate)   
+# Call the function here
+log("Extract Phase Started")
+extract_data=extract()
+# Print the rows here
+extract_data.head(5)
